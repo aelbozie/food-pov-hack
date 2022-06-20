ea5d0ce156eafd42e0637ce23c557013bec06147
aelbozie <36151122+aelbozie@users.noreply.github.com>
2022-06-20 11:51:23 +0100

    fix: ports

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 export INITIALISE_WITH_MOCK_DATA=1
-export FASTAPI_PORT=${PORT:-8000}
+export FASTAPI_PORT=${8000}
 export STREAMLIT_PORT=${PORT:-8001}
 
 rm -f data/inventory.db
 
-uvicorn service.main:app --proxy-headers --port $FASTAPI_PORT --host 0.0.0.0 &
-PYTHONPATH="." streamlit run web/1_Barcode_Scanner.py --server.port $STREAMLIT_PORT --server.address 0.0.0.0
+PYTHONPATH="." streamlit run web/1_Barcode_Scanner.py --server.port $STREAMLIT_PORT --server.address 0.0.0.0 &&
+uvicorn service.main:app --proxy-headers --port $FASTAPI_PORT --host 0.0.0.0 
+
